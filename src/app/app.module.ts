4326175307c60e32cd89ebf52ef68044a8c98503
Rohan Gupta <sitcomdeveloper43@gmail.com>
2020-08-31 00:36:03 +0530

    Rohan add files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { HeaderComponent } from './index/header/header.component';
+import { FooterComponent } from './index/footer/footer.component';
+import { HomeComponent } from './index/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
