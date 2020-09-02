181cbc1dace0c6038eb69ef32c139625d72de03d
Rohan Gupta <sitcomdeveloper43@gmail.com>
2020-09-02 15:58:55 +0530

    Rohan integrate all the API

--- src/app/index/header/userhome/userhome.component.ts
+++ src/app/index/header/userhome/userhome.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ServicesService } from 'src/app/services.service';
 
 @Component({
   selector: 'app-userhome',
@@ -11,33 +13,126 @@ export class UserhomeComponent implements OnInit {
   chngpwd = false;
   cntctus = false;
 
+  dpsitfunds = false;
   trdinghistry = false;
   uplddocuments = false;
   mnetrytransction = false;
   withdrwl = false;
-  constructor() { }
+  UserFormInfo: FormGroup
+  detailsonEmail: any;
+  updateddetailsofuser: any;
+  updatedpasswrd: any;
+  response: string;
+  usercontactus: any;
+  fundtheAccount: any;
+  constructor(private fb: FormBuilder, private service: ServicesService) { }
 
   ngOnInit(): void {
+    this.UserFormInfo = this.fb.group({
+      // contact us
+      firstname: [''],
+      lastname: [''],
+      countryname: [''],
+      email: [''],
+      phonecode: [''],
+      phonenumber: [''],
+      subject: [''],
+      message: [''],
+      // deposit funds
+      trdeaccount: [''],
+      amount: [''],
+      // chngepwd
+      oldpwd: [''],
+      newpwd: [''],
+      confirmpwd: [''],
+    })
+    this.getdetailsofUser();
+  }
+  // fund account
+  addfund() {
+    const fndaccntparamtr = {
+      TPAccountNumber: this.UserFormInfo.value.trdeaccount,
+      DepositAmount: this.UserFormInfo.value.amount,
+    }
+    this.service.fundAccount(fndaccntparamtr).subscribe(giveFund => {
+      this.fundtheAccount = giveFund;
+      console.log('fundtheAccount',giveFund);
+    })
+  }
+  // fetch personal details
+  getdetailsofUser() {
+    // this.service.fetchpersonaldetails().subscribe(dtlsoffetchuser => {
+    //   this.detailsonEmail = dtlsoffetchuser;
+    //   console.log('detailsonEmail',dtlsoffetchuser);
+    // })
+  }
+  // update details
+  updatedetailsforemailUser() {
+    // const updtprsnldetls = {}
+    // this.service.updatepersonaldetails(updtprsnldetls).subscribe(updateddetails => {
+    //   this.updateddetailsofuser = updateddetails;
+    //   console.log('updateddetailsofuser',updateddetails);
+    // })
+  }
+  // update password
+  updatethepassword() {
+    const chngepwdparamtr = {
+      Id: 1,
+      Email: 1,
+      OldPassword: this.UserFormInfo.value.oldpwd,
+      NewPassword: this.UserFormInfo.value.newpwd,
+      ConfirmPassword: this.UserFormInfo.value.confirmpwd,
+    }
+    this.service.changePassword(chngepwdparamtr).subscribe(updtpwdbyeml => {
+      this.updatedpasswrd = updtpwdbyeml;
+      if (updtpwdbyeml === 'null') {
+        this.response = 'Password is updated successfully..!';
+      } else {
+        this.response = '';
+      }
+      this.UserFormInfo.reset();
+      console.log('updatedpasswrd', updtpwdbyeml);
+    })
+  }
+  // contact us
+  fillcontactusform() {
+    const contctusParamtr = {
+      OwnerId: 1,
+       FirstName: this.UserFormInfo.value.firstname,
+       LastName: this.UserFormInfo.value.lastname,
+       Email: this.UserFormInfo.value.email,
+       Phone: this.UserFormInfo.value.phonecode + this.UserFormInfo.value.phonenumber, 
+      CountryId: '', 
+       CountryName: this.UserFormInfo.value.countryname,
+       Subject: this.UserFormInfo.value.subject,
+       Message: this.UserFormInfo.value.message,
+    }
+    this.service.contactUs(contctusParamtr).subscribe(contctusforquery => {
+      this.usercontactus = contctusforquery;
+      console.log('usercontactus', contctusforquery);
+    })
   }
   userhome() {
-this.home = true;
-this.personalddetails = false;
-this.chngpwd = false;
-this.cntctus = false;
-this.trdinghistry = false;
-this.uplddocuments = false;
-this.mnetrytransction = false;
-this.withdrwl = false;
+    this.home = true;
+    this.personalddetails = false;
+    this.chngpwd = false;
+    this.cntctus = false;
+    this.trdinghistry = false;
+    this.uplddocuments = false;
+    this.mnetrytransction = false;
+    this.withdrwl = false;
+    this.dpsitfunds = false;
   }
   prsnldtls() {
     this.home = false;
-this.personalddetails = true;
-this.chngpwd = false;
-this.cntctus = false;
-this.trdinghistry = false;
-this.uplddocuments = false;
-this.mnetrytransction = false;
-this.withdrwl = false;
+    this.personalddetails = true;
+    this.chngpwd = false;
+    this.cntctus = false;
+    this.trdinghistry = false;
+    this.uplddocuments = false;
+    this.mnetrytransction = false;
+    this.withdrwl = false;
+    this.dpsitfunds = false;
   }
   chngepassword() {
     this.home = false;
@@ -45,9 +140,10 @@ this.withdrwl = false;
     this.chngpwd = true;
     this.cntctus = false;
     this.trdinghistry = false;
-this.uplddocuments = false;
-this.mnetrytransction = false;
-this.withdrwl = false;
+    this.uplddocuments = false;
+    this.mnetrytransction = false;
+    this.withdrwl = false;
+    this.dpsitfunds = false;
   }
   contactwe() {
     this.home = false;
@@ -55,9 +151,10 @@ this.withdrwl = false;
     this.chngpwd = false;
     this.cntctus = true;
     this.trdinghistry = false;
-this.uplddocuments = false;
-this.mnetrytransction = false;
-this.withdrwl = false;
+    this.uplddocuments = false;
+    this.mnetrytransction = false;
+    this.withdrwl = false;
+    this.dpsitfunds = false;
   }
   tradinghistory() {
     this.home = false;
@@ -65,19 +162,21 @@ this.withdrwl = false;
     this.chngpwd = false;
     this.cntctus = false;
     this.trdinghistry = true;
-this.uplddocuments = false;
-this.mnetrytransction = false;
-this.withdrwl = false;
+    this.uplddocuments = false;
+    this.mnetrytransction = false;
+    this.withdrwl = false;
+    this.dpsitfunds = false;
   }
   depositfunds() {
     this.home = false;
     this.personalddetails = false;
     this.chngpwd = false;
-    this.cntctus = true;
+    this.cntctus = false;
     this.trdinghistry = false;
-this.uplddocuments = false;
-this.mnetrytransction = false;
-this.withdrwl = false;
+    this.uplddocuments = false;
+    this.mnetrytransction = false;
+    this.withdrwl = false;
+    this.dpsitfunds = true;
   }
   uploaddocuments() {
     this.home = false;
@@ -85,9 +184,10 @@ this.withdrwl = false;
     this.chngpwd = false;
     this.cntctus = false;
     this.trdinghistry = false;
-this.uplddocuments = true;
-this.mnetrytransction = false;
-this.withdrwl = false;
+    this.uplddocuments = true;
+    this.mnetrytransction = false;
+    this.withdrwl = false;
+    this.dpsitfunds = false;
   }
   monetarytransactions() {
     this.home = false;
@@ -95,9 +195,10 @@ this.withdrwl = false;
     this.chngpwd = false;
     this.cntctus = false;
     this.trdinghistry = false;
-this.uplddocuments = false;
-this.mnetrytransction = true;
-this.withdrwl = false;
+    this.uplddocuments = false;
+    this.mnetrytransction = true;
+    this.withdrwl = false;
+    this.dpsitfunds = false;
   }
   withdrawl() {
     this.home = false;
@@ -105,8 +206,9 @@ this.withdrwl = false;
     this.chngpwd = false;
     this.cntctus = false;
     this.trdinghistry = false;
-this.uplddocuments = false;
-this.mnetrytransction = false;
-this.withdrwl = true;
+    this.uplddocuments = false;
+    this.mnetrytransction = false;
+    this.withdrwl = true;
+    this.dpsitfunds = false;
   }
 }
--- src/app/services.service.ts
+++ src/app/services.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { EmailValidator } from '@angular/forms';
 
 const API_URL = environment.API_URL;
 @Injectable({
@@ -16,14 +17,22 @@ export class ServicesService {
   }
   // fund account
   fundAccount(fndaccntparamtr: any): Observable<any> {
-    return this.http.post<any>(API_URL + 'TradeAccount/AddDeposit',fndaccntparamtr);
+    return this.http.post<any>(API_URL + 'TradeAccount/AddDeposit', fndaccntparamtr);
+  }
+  // get personal details
+  fetchpersonaldetails(email: any): Observable<any> {
+    return this.http.post<any>(API_URL + 'Client/GetTradeAccountDetailWithAddressById/' + email, {})
+  }
+  // update perosnal details
+  updatepersonaldetails(updtprsnldetls: any): Observable<any> {
+    return this.http.post<any>(API_URL + 'Client/UpdateClientWithAddress', updtprsnldetls);
   }
   // chnge pwd
   changePassword(chngepwdparamtr: any): Observable<any> {
-    return this.http.post<any>(API_URL + 'Client/UpdatePasswordOfClient',chngepwdparamtr);
+    return this.http.post<any>(API_URL + 'Client/UpdatePasswordOfClient', chngepwdparamtr);
   }
   // contact us
   contactUs(contctusParamtr: any): Observable<any> {
-    return this.http.post<any>(API_URL + 'Client/InsertClientQuery',contctusParamtr);
+    return this.http.post<any>(API_URL + 'Client/InsertClientQuery', contctusParamtr);
   }
 }
